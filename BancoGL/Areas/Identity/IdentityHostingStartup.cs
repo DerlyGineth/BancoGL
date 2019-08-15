949dbb2282ea14c577203028bc8612dac04fa34c
DerlyGineth <54144244+DerlyGineth@users.noreply.github.com>
2019-08-15 11:53:27 -0500

    Initial commit

---
+++ BancoGL/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BancoGL.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BancoGL.Areas.Identity.IdentityHostingStartup))]
+namespace BancoGL.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ BancoGL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BancoGL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
